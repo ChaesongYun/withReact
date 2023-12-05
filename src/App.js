00ad646b5eb982f7c15e506355b27013eb284f19
DESKTOP-KKKEMOT\최작가 <hapbbie@naver.com>
2023-12-05 19:46:43 +0900

    1205 리액트연결

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  // let name = 'hapbbie';
-  let [name, setName] = useState('hapbbie');
-  return (
-    <div className="App">
-      <header className="App-header">
-      <div>{name}</div>
-      </header>
+const App = () => {
+  // const [text, setText] = useState("");
+  // const [edit, setEdit] = useState(false);
+
+  // let content = <div>{text}</div>
+
+  // if(edit){
+  //   content = <div>
+  //   <input type="text" 
+  //     value={text}
+  //     onChange={(e)=>{
+  //     setText(e.target.value);
+  //     console.log(e.target.value);
+  //     }}
+  //   />
+  // </div>
+  // }
+  // return(
+  //   <div>
+  //     <div>{content}</div>
+  //     <button onClick={()=>(setEdit(!edit))}>수정</button>
+  //   </div>
+  // )
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    axios({
+      method:'GET', 
+      url:'https://jsonplaceholder.typicode.com/posts'
+    }).then((res)=>{
+      setPosts(res.data);
+    })
+  })
+  return(
+    <div>
+      <ul>
+        {posts.map(post=>{
+          <li key={post.id}>{post.title}</li>
+        })}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
